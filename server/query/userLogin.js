679e69d826be96866d3a272d688950c532738888
H3CO3 <52942896+H3CO3@users.noreply.github.com>
2020-04-17 22:44:37 +0800

    Update userLogin.js

--- server/query/userLogin.js
+++ server/query/userLogin.js
@@ -3,47 +3,45 @@
  * 输入账号和密码→数据库查询账号密码是否存在→跳转登录(3)
  * 数据库返回user(obj)
  * get: id & key
- * return: error code / name
+ * return: users_(boolean)
  */
-const axios = require("axios");
-
-const login = function(info) {
-	axios.post(
-	'https://afusuj.toutiao15.com/login', 
-	{	
+const login = async function(info) {
+	await axios.post(
+		'https://afusuj.toutiao15.com/login',  //url,"插入某个用户于DB"的云函数的网址
+		{	
             id: info.id,
             key: info.key,
-	}
+		}
 	).then(function(response){
-	let status = response.data;	    
+		let status = response.data;	    //sta为response的key值，根据云函数修改
         if (status == 0)
         {
-	    /**
-	     * 云端错误码0，账号与密码不存在或不匹配
-	     * 返回错误码400
-	     */
-            console.log(0);
+            /**
+             * 用户名或密码错误
+             * code:400
+             */
+            console.log(400);
             return 400;
         }
         else if (status == -1)
         {
-	    /**
-	     * 云端错误码-1，账号已登录，不能重登录
-	     * 返回错误码388
-	     */
-            console.log(-1);
-            return 388;
+            /**
+             * 用户已登录
+             * code:403
+             */
+            console.log(403);
+            return 403;
         }
         else
         {
-	    /**
-	     * 正常登录
-	     * 返回账号昵称，正确码&其他信息待添加
-	     */
-            console.log(status);
+            /**
+             * 登录成功
+             * code:200
+             */
+            console.log(status + " 200");
             return status;
         }
 	})
 };
-module.exports = login;
 
+//module.exports = login;
