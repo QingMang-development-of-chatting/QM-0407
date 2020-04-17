bed00fb16b387d5fa07a82c3c8af8fee402a6f95
H3CO3 <52942896+H3CO3@users.noreply.github.com>
2020-04-17 22:48:23 +0800

    Update userLogout.js

--- server/query/userLogout.js
+++ server/query/userLogout.js
@@ -3,31 +3,43 @@
  * 和登录一模一样的冗余代码
  * 待改
  */
-
-const logout = function(info) {
-	axios.post(
-		'https://afusuj.toutiao15.com/logout',  //url,"插入某个用户于DB"的云函数的网址
-		{	
+const logout = async function(info) {
+    await axios.post(
+        'https://afusuj.toutiao15.com/logout',
+        {    
             id: info.id,
             key: info.key,
-		}
-	).then(function(response){
-		let status = response.data;	    //sta为response的key值，根据云函数修改
+        }
+    ).then(function(response){
+        let status = response.data;
         if (status == 0)
         {
-            console.log(0);
-            return 0;
+            /**
+             * 用户名或密码错误
+             * code:400
+             */
+            console.log(400);
+            return 400;
         }
         else if (status == -1)
         {
-            console.log(-1);
-            return -1;
+             /**
+             * 用户未登录
+             * code:403
+             */
+            console.log(403);
+            return 403;
         }
         else
         {
-            console.log(status);
+            /**
+             * 登出成功
+             * code:200
+             */
+            console.log(status + " 200");
             return status;
         }
-	})
+    })
 };
 
+//module.exports = logout;
