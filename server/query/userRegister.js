c07f9ca350690ce59e29df3afb0bd9361d36a055
H3CO3 <52942896+H3CO3@users.noreply.github.com>
2020-04-23 00:42:39 +0800

    Update userRegister.js

--- server/query/userRegister.js
+++ server/query/userRegister.js
@@ -1,8 +1,16 @@
 /**
  * 用户注册
  * 用户输入账号密码→数据库检测账号密码是否已存在(unique)→不存在则插入账号信息(insert)→页面跳转
- * get: info{id, key, name}
+ * 
+ * 输入：
+ * --info.id (string) 用户账号
+ *   info.key (string) 用户密码
+ *   info.name (string) 用户名字
  * return: 
+ * --false: 奇怪的bug
+ * --400：账号重名
+ * --result.id: 用户账号
+ *   result.name: 用户名
  */
 const axios = require('axios');
 const insertUser = async function(info) {
@@ -25,7 +33,7 @@ const insertUser = async function(info) {
              * code:400
              */
             //console.log("error:" + status + " 400");
-            result = false;
+            result = 400;
         }
         else
         {
