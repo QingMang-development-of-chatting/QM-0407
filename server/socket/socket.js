a40bef1b1fa15b9be68bd245e94ae05c7f45e376
ZengJia-Notes <55644002+ZengJia-Notes@users.noreply.github.com>
2020-04-22 22:12:13 +0800

    Add socket via upload

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,1 +1,17 @@
+const Server = require('socket.io');
 
+const io = new Server();
+
+const chat = io.of('/chat');
+
+chat.on('connection', socket => {
+	console.log(`${socket.id} connected`);
+	socket.join('public', () => {
+		socket.on('v1/message', msg => {
+			socket.to('public').broadcast.emit('v1/message', msg);
+		});
+	});
+
+});
+
+module.exports = io;
