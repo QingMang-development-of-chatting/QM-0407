2ea7cb79a55ddf6cfe079a4ca6a38e0a41048d90
Weiyi Yao <eluc@qq.com>
2020-04-25 23:18:37 +0800

    小改

--- server/service/msgService.js
+++ server/service/msgService.js
@@ -7,14 +7,13 @@ const MsgService = {};
 /**
  * msg直接存入DB。暂时时来一条传一条。
  * TODO：增加缓存
- * @param {String} id
- * @param {object} msg      eg.{"sender":"0002","room_id":"1","text":"test"}
+ * @param {String} room_id
+ * @param {object} msg      eg.{"sender":"0002","text":"test"}
  * @return {Boolean}
  */
-MsgService.insertChat = async function (msg){
+MsgService.insertChat = async function (room_id,msg){
     let sender = msg.sender;
     let text = msg.text;
-    let room_id = msg.room_id;
     var status = await insertChat({"id":sender,"room_id":room_id,"chat":[text]});	
     if(!status){
         return false;
