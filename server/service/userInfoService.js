d66f24c1310ae9455da16f9ccf33390143d3d279
Weiyi Yao <eluc@qq.com>
2020-05-24 11:36:11 +0800

    Update userInfoService.js

--- server/service/userInfoService.js
+++ server/service/userInfoService.js
@@ -1,11 +1,12 @@
 //Module Dependencies
 const searchUser = require('../query2.0/searchUser.js');
+const updUser = require('../query2.0/updUser.js');
 
 //API
 const UserInfoService = {};
 
 /**
- * TODO 获取多个账号的信息
+ * 获取多个账号的信息
  * @param   {Array}   user_id_array
  * @return  {Array}   user_info_array
  */
@@ -19,24 +20,33 @@ UserInfoService.getInfoByArray= async function (user_id_array) {
 };
 
 /**
- * TODO update昵称(user_name)
+ * update昵称(user_name)
  * @param   {String}     user_id
  * @param   {String}     user_name
  * @return  {Boolean}    result
  */
 UserInfoService.updateName= async function (user_id, user_name) {
-
+	var result = await updUser({"user_id":user_id,"user_name":user_name});
+	if(result!=402&&result!=false){
+		return true;
+	}
+	return false;
 };
 
 /**
- * TODO update头像
+ * update头像
  * @param   {String}     user_id
- * @param   {String}     user_name
+ * @param   {String?}     user_photo
  * @return  {Boolean}    result
  */
-UserInfoService.updatePhoto= async function (user_id, user_name) {
-
+UserInfoService.updatePhoto= async function (user_id, user_photo) {
+	var result = await updUser({"user_id":user_id,"user_photo":user_photo});
+	if(result!=402&&result!=false){
+		return true;
+	}
+	return false;
 };
 
 module.exports = UserInfoService;
 //UserInfoService.getInfoByArray(["0080","test2","0090"]);
+//UserInfoService.updateName("0080",1234);
