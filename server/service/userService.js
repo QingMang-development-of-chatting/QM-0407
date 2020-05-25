89aa3981928adf16e8c84e809f87eaf2a19b7040
Weiyi Yao <eluc@qq.com>
2020-05-25 16:55:10 +0800

    Update userService.js
    使用了query2.0的内容

--- server/service/userService.js
+++ server/service/userService.js
@@ -1,27 +1,13 @@
 //Module Dependencies
-const login = require('../query/userLogin.js');
-const logout = require('../query/userLogout.js');
-const insertUser = require('../query/userRegister.js');
+const logUser = require('../query2.0/logUser.js');
+const insertUser = require('../query2.0/insertUser.js');
 
 //API
 const UserService = {};
 
-/**
- * delay for MS ms
- * pretend to query the cloud DB.
- *
- * @param {Number} ms
- */
-const timeout = function(ms) {
-	return new Promise((resolve, reject) => {
-		setTimeout(resolve, ms);
-	})
-};
-
+//TODO 各类合法性检测
 //username合法性检测
 const checkUsername = function(username){
-	//TODO位数限制
-	//TODO特殊字符限制
 	return true;
 }
 //password合法性检测
@@ -36,18 +22,18 @@ const checkNickname = function(nickname){
 /**
  * register
  *
- * @param {String} username
- * @param {String} password
- * @param {String} nickname
- * @return {Boolean}
+ * @param {String} user_id		账号
+ * @param {String} user_key		密码
+ * @param {String} user_name	昵称
+ * @return {Boolean} status
  */
-UserService.register = async function (username, password, nickname) {
-	//TODO合法性检测
-	if(!(checkUsername(username)&&checkPassword(password)&&checkNickname(nickname))){
+UserService.register = async function (user_id, user_key, user_name) {
+	//合法性检测
+	if(!(checkUsername(user_id)&&checkPassword(user_key)&&checkNickname(user_name))){
 		return false;
 	}
-	//在DB注册用户
-	var status = await insertUser({"id":username,"key":password,"name":nickname});
+	//注册用户
+	var status = await insertUser({"user_id":user_id,"user_key":user_key,"user_name":user_name});
 	if(status!=400){
 		return true;
 	}
@@ -57,12 +43,12 @@ UserService.register = async function (username, password, nickname) {
 /**
  * login
  * 
- * @param {String} username
- * @param {String} password
+ * @param {String} user_id		账号
+ * @param {String} user_key		密码
  */
-UserService.login = async function (username, password) {
-	var status = await login({"id":username,"key":password});	
-	if(status!=400&&status!=403){
+UserService.login = async function (user_id, user_key) {
+	var status = await logUser({"id":user_id,"key":user_key});	
+	if(status!=401&&status!=402&&status!=403){
 		return true;
 	}
 	return status;
@@ -70,17 +56,18 @@ UserService.login = async function (username, password) {
 
 /**
  * logout
- * 
- * @param {String} username
- * @param {String} password
+ * @param {String} user_id		账号
  * @return {Boolean}
  */
-UserService.logout = async function (username, password) {
-	var status = await logout({"id":username,"key":password});
-	if(status!=400&&status!=403){
+UserService.logout = async function (user_id) {
+	var status = await logUser({"id":user_id});
+	if(status!=402&&status!=403){
 		return true;
 	}
 	return status;
 };
 
 module.exports = UserService;
+//UserService.register("test_user","test_key","test_name");
+//UserService.login("test_user","test_key");
+//UserService.logout("test_user");
