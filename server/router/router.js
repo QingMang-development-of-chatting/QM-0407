250d8ccbc2132ec5b5c140d6a6c344d8a7cea590
ZengJia-Notes <55644002+ZengJia-Notes@users.noreply.github.com>
2020-05-27 17:51:17 +0800

    update the route to user info

--- server/router/router.js
+++ server/router/router.js
@@ -3,23 +3,39 @@
  */
 const compose = require('koa-compose');
 const Router = require('@koa/router');
-const controller = require('../controller/controller');
+const user_controller = require('../controller/user_controller');
+const userinfo_controller = require('../controller/userinfo_controller');
 
 const router = new Router();
 
 /**
  * register router middleware
  */
-router.post('/register', controller.register);
+router.post('/register', user_controller.register);
 
 /**
  * login router middleware
  */
-router.post('/login', controller.login);
+router.post('/login', user_controller.login);
 
 /**
  * logout router middleware
  */
-router.get('/logout', controller.logout);
+router.get('/logout', user_controller.logout);
+
+/**
+ * Get users infos
+ */
+router.post('/userinfo', userinfo_controller.getUsers);
+
+/**
+ * Update the nickname of a user
+ */
+router.put('/userinfo/nickname', userinfo_controller.updateNickname);
+
+/**
+ * Update the photo of a user
+ */
+router.put('/userinfo/photo', userinfo_controller.updatePhoto);
 
 module.exports = router;
