3c7f4e1f03ca48c85f21f71992802d508da1f158
ZengJia-Notes <55644002+ZengJia-Notes@users.noreply.github.com>
2020-05-27 15:43:02 +0800

    support 0427 events

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,31 +1,69 @@
 const Server = require('socket.io');
-const service = require('../service/msgService');
+// const service = require('../service/msgService');
 
 const io = new Server();
 
 const chat = io.of('/chat');
 
-// chat.on('connection', socket => {
-// 	console.log(`${socket.id} connected`);
-// 	socket.join('public', () => {
-// 		socket.on('v1/message', msg => {
-// 			socket.to('public').broadcast.emit('v1/message', msg);
-// 			service.insertChat(msg);
-// 		});
-// 	});
-// });
+const user_socket = new Map();
+const socket_user = new Map();
 
 chat.on('connection', socket => {
 	console.log(`${socket.id} connected`);
-	socket.on('v1/join room', room_id => {
-		socket.join(room_id);
+
+	socket.on('login', username => {
+		let socket_id = user_socket.get(username);
+		if (socket_id) {
+			socket.to(socket_id).emit('logout', 'another device');
+			socket_user.delete(socket_id);
+		}
+		user_socket.set(username, socket.id);
+		socket_user.set(socket.id, username);
+		socket.emit('login');
 	});
-	socket.on('v1/leave room', room_id => {
-		socket.leave(room_id);
+
+	socket.on('logout', () => {
+		let username = socket_user.get(socket.id);
+		socket_user.delete(socket.id);
+		user_socket.delete(username);
+		socket.emit('logout', 'regular');
 	});
-	socket.on('v1.1/message', (room_id, msg) => {
-		socket.to(room_id).broadcast.emit('v1.1/message', room_id, msg);
-		service.insertChat(room_id, msg);
+
+	socket.on('message', (friend, text) => {
+		let friend_socket = user_socket.get(friend);
+		if (friend_socket) {
+			socket.to(friend_socket).emit('message', socket_user.get(socket.id), text);
+		}
+	});
+
+	socket.on('v1/friend/add', friend => {
+		let friend_socket = user_socket.get(friend);
+		if (friend_socket) {
+			socket.to(user_socket.get(friend)).emit('v1/friend/add', socket_user.get(socket.id));			
+		}
+	});
+
+	socket.on('v1/friend/remove', friend => {
+		let friend_socket = user_socket.get(friend);
+		if (friend_socket) {
+			socket.to(user_socket.get(friend)).emit('v1/friend/remove', socket_user.get(socket.id));			
+		}
+	});
+
+	socket.on('v1/friend/access', friend => {
+		let friend_socket = user_socket.get(friend);
+		if (friend_socket) {
+			socket.to(user_socket.get(friend)).emit('v1/friend/access', socket_user.get(socket.id));			
+		}
+	});
+
+	socket.on('disconnect', reason => {
+		console.log(reason);
+		let username = socket_user.get(socket.id);
+		if (username) {
+			user_socket.delete(username);
+		}
+		socket_user.delete(socket.id);
 	});
 });
 
