ca845230b933e66acb8164780a6ee8752836243c
H3CO3 <52942896+H3CO3@users.noreply.github.com>
2020-05-28 15:56:49 +0800

    Update searchChat.js

--- server/query2.0/searchChat.js
+++ server/query2.0/searchChat.js
@@ -8,12 +8,15 @@
  *   可选：
  *   info.key (string) 关键字        ————按关键字搜索
  *   info.date (Date.getTime) 时间   ————按时间搜索
+ *   info.new 如果=1，则输出一条此room的最新消息
  * 输出：
  * --0：没有查到记录
  * --[{host_id:"id", chat:"chat", data:"Date.getTime"}...]
  *   result[i].host_id 用户ID
  *   result[i].chat 信息
  *   result[i].date 需要转换回去的自1970年1月1日以来的毫秒数
+ *   result[i].user_read 已经阅读本消息的人
+ * --搜索第一条记录，返回的不是列表，其余同↑
  */
 
 const axios = require('axios');
