574c3c9e742db164a060a910d7235c9f644a7c80
ZengJia-Notes <55644002+ZengJia-Notes@users.noreply.github.com>
2020-06-03 23:49:17 +0800

    Add files via upload

--- simulate/http/router.userinfo.js
+++ simulate/http/router.userinfo.js
@@ -11,14 +11,18 @@ const router = new Router();
 // sets routes, get user's basic infomation
 router.get(HTTP.V1.USERINFO.GET_INFO, async ctx => {
 	const { username } = ctx.params;
+
+	const is_valid = typeof(username) === 'string' && username !== ':username';
+
+	if (!is_valid) {
+		ctx.status = 400;
+		return;
+	}
+
 	const result = await user_service.getInfo(username);
 	if (result.status === STATUS.OK) {
 		ctx.status = 200;
 		ctx.body = result.data;
-	}
-	else if (result.status === STATUS.BAD_PARAM) {
-		ctx.status = 400;
-		ctx.body = result.reason;
 	}
 	else if (result.status === STATUS.NOT_FOUND) {
 		ctx.status = 404;
@@ -30,16 +34,21 @@ router.put(HTTP.V1.USERINFO.SET_PASSWORD, async ctx => {
 	const { username } = ctx.params;
 	const { password } = ctx.request.body;
 
+	const is_valid_username = typeof(username) === 'string' && username !== ':username';
+	const is_valid_password = typeof(password) === 'string';
+	const is_valid = is_valid_username && is_valid_password;
+
+	if (!is_valid) {
+		ctx.status = 400;
+		return;
+	}
+
 	const result = await user_service.setPassword(username, password);
 	if (result.status === STATUS.OK) {
 		ctx.status = 200;
 	}
-	else if (result.status === STATUS.BAD_PARAM) {
-		ctx.status = 400;
-		ctx.body = result.reason;
-	}
-	else if (result.status === STATUS.REJECT) {
-		ctx.status = 408;
+	else if (result.status === STATUS.NOT_FOUND) {
+		ctx.status = 404;
 	}
 });
 
@@ -48,16 +57,21 @@ router.put(HTTP.V1.USERINFO.SET_NICKNAME, async ctx => {
 	const { username } = ctx.params;
 	const { nickname } = ctx.request.body;
 
+	const is_valid_username = typeof(username) === 'string' && username !== ':username';
+	const is_valid_nickname = typeof(nickname) === 'string';
+	const is_valid = is_valid_username && is_valid_nickname;
+
+	if (!is_valid) {
+		ctx.status = 400;
+		return;
+	}
+
 	const result = await user_service.setNickname(username, nickname);
 	if (result.status === STATUS.OK) {
 		ctx.status = 200;
 	}
-	else if (result.status === STATUS.BAD_PARAM) {
-		ctx.status = 400;
-		ctx.body = result.reason;
-	}
-	else if (result.status === STATUS.REJECT) {
-		ctx.status = 408;
+	else if (result.status === STATUS.NOT_FOUND) {
+		ctx.status = 404;
 	}
 });
 
@@ -66,16 +80,21 @@ router.put(HTTP.V1.USERINFO.SET_PHOTO, async ctx => {
 	const { username } = ctx.params;
 	const { photo } = ctx.request.body;
 
+	const is_valid_username = typeof(username) === 'string' && username !== ':username';
+	const is_valid_photo = typeof(photo) === 'string';
+	const is_valid = is_valid_username && is_valid_photo;
+
+	if (!is_valid) {
+		ctx.status = 400;
+		return;
+	}
+
 	const result = await user_service.setPhoto(username, photo);
 	if (result.status === STATUS.OK) {
 		ctx.status = 200;
 	}
-	else if (result.status === STATUS.BAD_PARAM) {
-		ctx.status = 400;
-		ctx.body = result.reason;
-	}
-	else if (result.status === STATUS.REJECT) {
-		ctx.status = 408;
+	else if (result.status === STATUS.NOT_FOUND) {
+		ctx.status = 404;
 	}
 });
 
