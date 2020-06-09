601694c206d16f9ed8c4f4eb003847ae84deb1e1
Weiyi Yao <eluc@qq.com>
2020-06-09 20:10:22 +0800

    Update test.service.user.js
    修改了测试样例的一些错误

--- server/test/test.service.user.js
+++ server/test/test.service.user.js
@@ -5,6 +5,8 @@
  *   This test will create a user called `test_user` in database.
  */
 
+//mocha -t 5000 test.service.user.js
+
 /**
  * Module dependencies
  */
@@ -16,51 +18,54 @@ const user_service = new UserService();
 
 describe('User', function() {
 	describe('#register()', function() {
+		//username:3~15 不能包含_-之外的特殊字符
+		//pw:8~16
+		//nickname:1~10
 		it('should response REJECT for type error', async function() {
 			this.timeout(50000);
 			// username too short
-			const result_username_too_short = await user_service.register('12', 'test_password', 'test_nickname');
+			const result_username_too_short = await user_service.register('12', 'test_password', 'test');
 			expect(result_username_too_short.status).to.eql(STATUS.REJECT);
 			expect(result_username_too_short.reason).to.eql(REASON.REGISTER.USERNAME_ERROR);
 
 			// username too long
-			const result_username_too_long = await user_service.register('1234567890123456', 'test_password', 'test_nickname');
+			const result_username_too_long = await user_service.register('1234567890123456', 'test_password', 'test');
 			expect(result_username_too_long.status).to.eql(STATUS.REJECT);
 			expect(result_username_too_long.reason).to.eql(REASON.REGISTER.USERNAME_ERROR);
 
 			// username with characters deposed
-			const result_username_char = await user_service.register('#123', 'test_password', 'test_nickname');
+			const result_username_char = await user_service.register('#123', 'test_password', 'test');
 			expect(result_username_char.status).to.eql(STATUS.REJECT);
 			expect(result_username_char.reason).to.eql(REASON.REGISTER.USERNAME_ERROR);
 
 			// password too short
-			const result_password_too_short = await user_service.register('123456789012345', '1234567', 'test_nickname');
+			const result_password_too_short = await user_service.register('123456789012345', '1234567', 'test');
 			expect(result_password_too_short.status).to.eql(STATUS.REJECT);
 			expect(result_password_too_short.reason).to.eql(REASON.REGISTER.PASSWORD_ERROR);
 
 			// empty nickname
 			const result_nickname_empty = await user_service.register('123', '12345678', '');
 			expect(result_nickname_empty.status).to.eql(STATUS.REJECT);
-			expect(result_nickname_empty.reason).to.eql(REASON.USERINFO.NICKNAME_ERROR);
+			expect(result_nickname_empty.reason).to.eql(REASON.REGISTER.NICKNAME_ERROR);
 
 			// all error
 			const result_all_error = await user_service.register('12', '1234567', '');
 			expect(result_all_error.status).to.eql(STATUS.REJECT);
 			expect(result_all_error.reason).to.eql(
-				REASON.REGISTER.USERNAME_ERROR | REASON.REGISTER.PASSWORD_ERROR | REASON.USERINFO.NICKNAME_ERROR);
+				REASON.REGISTER.USERNAME_ERROR | REASON.REGISTER.PASSWORD_ERROR | REASON.REGISTER.NICKNAME_ERROR);
 		});
 
 		it('should response OK', async function() {
 			this.timeout(50000);
 			// creates a new user called `test_user`
-			const result = await user_service.register('test_user', 'test_password', 'test_nickname');
+			const result = await user_service.register('test_user', 'test_password', 'testn');
 			expect(result.status).to.eql(STATUS.OK);
 		});
 
 		it('should response REJECT for user duplicate', async function() {
 			this.timeout(50000);
 			// the user called `test_user` is occupied
-			const result = await user_service.register('test_user', 'test_password', 'test_nickname');
+			const result = await user_service.register('test_user', 'test_password', 'testn');
 			expect(result.status).to.eql(STATUS.REJECT);
 			expect(result.reason).to.eql(REASON.REGISTER.USER_DUPLICATE);
 		});
@@ -70,7 +75,7 @@ describe('User', function() {
 		it('should response NOT FOUND', async function() {
 			this.timeout(50000);
 			// the user called `test_none` should not be created
-			const result = await user_service.isValid('test_none', 'test_password', 'test_nickname');
+			const result = await user_service.isValid('test_none', 'test_password');
 			expect(result.status).to.eql(STATUS.NOT_FOUND);
 		});
 
@@ -82,7 +87,7 @@ describe('User', function() {
 
 		it('should response REJECT', async function() {
 			this.timeout(50000);
-			const result = await user_service.isValid('test_user', 'test_error', 'test_nickname');
+			const result = await user_service.isValid('test_user', 'test_error');
 			expect(result.status).to.eql(STATUS.REJECT);
 		});
 	});
@@ -101,7 +106,7 @@ describe('User', function() {
 			expect(result.status).to.eql(STATUS.OK);
 
 			// expects photo is `''`, for no setPhoto() before
-			expect(result.data).to.eql({username: 'test_user', nickname: 'test_nickname', photo: ''});
+			expect(result.data).to.eql({username: 'test_user', nickname: 'testn', photo: ''});
 		});
 	});
 
@@ -116,7 +121,7 @@ describe('User', function() {
 		it('should response NOT FOUND', async function() {
 			this.timeout(50000);
 			// the user called `test_none` should not be created
-			const result = await user_service.setPassword('test_none', 'test_password_new');
+			const result = await user_service.setPassword('test_none', '123456789');
 			expect(result.status).to.eql(STATUS.NOT_FOUND);
 		});
 
@@ -145,18 +150,18 @@ describe('User', function() {
 		it('should response NOT FOUND', async function() {
 			this.timeout(50000);
 			// the user called `test_none` should not be created
-			const result = await user_service.setNickname('test_none', 'test_nickname_new');
+			const result = await user_service.setNickname('test_none', 'testn_new');
 			expect(result.status).to.eql(STATUS.NOT_FOUND);
 		});
 
 		it('should response OK and receive correct data', async function() {
 			this.timeout(50000);
 			// sets new nickname
-			const result = await user_service.setNickname('test_user', 'test_nickname_new');
+			const result = await user_service.setNickname('test_user', 'testn_new');
 			expect(result.status).to.eql(STATUS.OK);
 			// checks if vaild
 			const { data: { nickname: nickname } } = await user_service.getInfo('test_user');
-			expect(nickname).to.eql('test_nickname_new');
+			expect(nickname).to.eql('testn_new');
 		});
 	});
 
