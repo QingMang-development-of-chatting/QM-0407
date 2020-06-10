d80c8185a774c5b201811e7e60a2fe679bfe3adb
QuanfaYan <46955702+QuanfaYan@users.noreply.github.com>
2020-06-11 01:02:42 +0800

    完善了词云功能，增加聊天历史记录utcTime存储。。

--- frontend/src/store/chatInfo/state.js
+++ frontend/src/store/chatInfo/state.js
@@ -1,7 +1,7 @@
 const STATE = {
     //聊天记录字典
-    //{ id: [ {message:~, isFriend:~, isRead:~, time:~,activeRate:~},...],...}
-    //{好友账号:[ {信息内容：~， 是否好友发送:~, 是否已读：~，发送时间：~ ,积极的概率：~}， ... ] , ... }
+    //{ id: [ {message:~, isFriend:~, isRead:~, time:~,utcTime:~,activeRate:~},...],...}
+    //{好友账号:[ {信息内容：~， 是否好友发送:~, 是否已读：~，发送时间(字符串)：~ ,发送时间（数字）：~，积极的概率：~}， ... ] , ... }
     chatMessages:{}
 };
 
